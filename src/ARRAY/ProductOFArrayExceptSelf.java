f25a686a27a06076080b9c896a8d1fdf8f51b5e1
dhruv264965 <dhruv264965shukla@gmail.com>
2025-07-07 08:43:17 +0530

    comment

--- src/ARRAY/ProductOFArrayExceptSelf.java
+++ src/ARRAY/ProductOFArrayExceptSelf.java
@@ -1,4 +1,8 @@
 package ARRAY;
+
+import java.util.ArrayList;
+import java.util.List;
+
 //Given an array nums n integers, return an array answer such that answer[i] is equal to the product of all the elements of
 // nums except nums[i]
 //You must solve the problem without division and in O(n).
@@ -7,7 +11,19 @@ public class ProductOFArrayExceptSelf {
 //        return new int[nums];
 //    }
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) {   //[48,24,12,8]
+        int num[] = {1, 2, 4, 6};
+        List<Integer> list = new ArrayList<>();
+        int sum = 0;
+        int total_sum=0;
+        for (int i = 0; i < num.length; i++) {
+            for (int j = i + 1; j < num.length; j++) {
+                sum += num[j]*num[j];
+                total_sum=sum-num[i];
+                i++;
+            }
+            list.add(total_sum);
+        }
+        System.out.println(list);
     }
 }
--- src/StreamApi/Test.java
+++ src/StreamApi/Test.java
@@ -1,32 +1,44 @@
 package StreamApi;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Test {
     public static void main(String[] args) {
-        String str="bcdeaflmubjrec";
-        int count=0;
-        int i=0;
-        int j=i+1;
-        while (i<=j){
-            if(str.charAt(i)!=str.charAt(j)){
-                count++;
-                j++;
-                i--;
-            }
-        }
+        // brute force
+//        int maxLen=0;
+//        String str="abcdabcse";
+//        HashSet<Character>charSet=new HashSet<>();
 //        for(int i=0;i<str.length();i++){
-//            for(int j=i+1;j<str.length();j++){
-//                if(str.charAt(i)!=str.charAt(j)){
-//                    count++;
-//                    j++;
-//
-//
+//            for(int j=i;j<str.length();j++){
+//                if(charSet.contains(str.charAt(j))){
+//                    break;
 //                }
+//                charSet.add(str.charAt(j));
 //            }
+//            maxLen=Math.max(maxLen, charSet.size());
 //        }
-        System.out.println(count);
-        //System.out.println(collect);
+//        System.out.println(maxLen);
+
+
+        /// optimal using sliding window
+        int maxLen=0;
+        String str="pwwkew";
+        HashSet<Character>hp=new HashSet<>();
+        int left=0;
+        for(int right=0;right<str.length();right++){
+            char ch=str.charAt(right);
+            while (hp.contains(ch)){
+                 char leftChar=str.charAt(left);
+                 hp.remove(leftChar);
+                left++;
+            }
+            hp.add(ch);
+            maxLen=Math.max(maxLen,right-left+1);
+        }
+        System.out.println(maxLen);
     }
+
+
 }
